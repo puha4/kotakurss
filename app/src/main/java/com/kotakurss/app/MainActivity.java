8c093f4306b4284085ab608a61ab13c4cef642a3
Yana <Yana>
2016-03-02 22:11:04 +0200

    add data to new fragments

--- app/src/main/java/com/kotakurss/app/MainActivity.java
+++ app/src/main/java/com/kotakurss/app/MainActivity.java
@@ -1,65 +1,27 @@
 package com.kotakurss.app;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.webkit.WebView;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import com.kotakurss.app.adapter.FeedMessageAdapter;
+
 import com.kotakurss.app.fragment.FeedListFragment;
 import com.kotakurss.app.fragment.FragmentChangeListener;
 import com.kotakurss.app.fragment.ViewerFragment;
-import com.kotakurss.app.rss.Feed;
-import com.kotakurss.app.rss.FeedMessage;
-import com.kotakurss.app.rss.RSSFeedParser;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends FragmentActivity implements FragmentChangeListener {
 
-    private static final String RSS_URL = "http://kotaku.com/rss";
-    private Feed feed;
-    private ListView rssListView;
-    private WebView webView;
-    private EditText inputSearch;
-    private FeedMessageAdapter messageAdapter;
-
-    private FeedListFragment feedListFragment;
-    private ViewerFragment viewerFragment;
-
     private FragmentManager manager;
-    private FragmentTransaction transaction;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        feedListFragment = new FeedListFragment();
-        viewerFragment = new ViewerFragment();
         manager = getSupportFragmentManager();
-
-
-//        webView = (WebView) findViewById(R.id.webView);
-//        webView.getSettings().setJavaScriptEnabled(true);
-
-//        initRssList();
-//        initSearch();
 
         if(savedInstanceState == null) {
             manager.beginTransaction().add(R.id.container, new FeedListFragment()).commit();
@@ -69,70 +31,12 @@ public class MainActivity extends FragmentActivity implements FragmentChangeList
     @Override
     public void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();;
+
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit);
         fragmentTransaction.replace(R.id.container, fragment, fragment.toString());
         fragmentTransaction.addToBackStack(fragment.toString());
         fragmentTransaction.commit();
-    }
-
-    private void initRssList() {
-
-        rssListView = (ListView) findViewById(R.id.rssListView);
-        feed = getFeed();
-
-        List<FeedMessage> feedMessagesList = feed.getMessages();
-
-        messageAdapter = new FeedMessageAdapter(this, feedMessagesList);
-
-        rssListView.setAdapter(messageAdapter);
-
-        rssListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                FeedMessage itemAtPosition = (FeedMessage) parent.getItemAtPosition(position);
-//                Log.i("MainActivity", "" + itemAtPosition);
-//                webView.loadUrl(itemAtPosition.getLink());
-
-//                Fragment f = new ViewerFragment();
-//
-//                transaction = manager.beginTransaction();
-//                transaction.setCustomAnimations(R.anim.enter, R.anim.exit, R.anim.pop_enter, R.anim.pop_exit);
-//                transaction.replace(R.id.feedlist_fragment, f);
-//                transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-//                transaction.addToBackStack(null);
-//                transaction.commit();
-            }
-        });
-    }
-
-    private void initSearch() {
-        inputSearch = (EditText) findViewById(R.id.searchInput);
-
-        inputSearch.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                messageAdapter.getFilter(s.toString());
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {}
-        });
-    }
-
-    private Feed getFeed() {
-        try {
-            feed = new RSSFeedParser().execute(RSS_URL).get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-
-        return feed;
     }
 
     @Override
--- app/src/main/java/com/kotakurss/app/fragment/FeedListFragment.java
+++ app/src/main/java/com/kotakurss/app/fragment/FeedListFragment.java
@@ -1,48 +1,103 @@
 package com.kotakurss.app.fragment;
 
-
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
+
 import com.kotakurss.app.R;
+import com.kotakurss.app.adapter.FeedMessageAdapter;
+import com.kotakurss.app.rss.Feed;
+import com.kotakurss.app.rss.FeedMessage;
+import com.kotakurss.app.rss.RSSFeedParser;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class FeedListFragment extends Fragment {
-    private ListView listView;
+    private ListView rssListView;
+    private Feed feed;
+    private View view;
+    private FeedMessageAdapter messageAdapter;
+    private EditText inputSearch;
+
+    private static final String RSS_URL = "http://kotaku.com/rss";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.feedlist_fragment, container, false);
+        view = inflater.inflate(R.layout.feedlist_fragment, container, false);
 
-        // определяем массив типа String
-        listView = (ListView) view.findViewById(R.id.rssListView);
-        final String[] catnames = new String[] {
-                "Рыжик", "Барсик", "Мурзик"
-        };
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_list_item_1, catnames);
-
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                showOtherFragment();
-            }
-        });
-
+        initRssList();
+        initSearch();
 
         return view;
     }
 
-    public void showOtherFragment()
+    private void initRssList() {
+        rssListView = (ListView) view.findViewById(R.id.rssListView);
+        feed = getFeed();
+
+        List<FeedMessage> feedMessagesList = feed.getMessages();
+
+        messageAdapter = new FeedMessageAdapter(this.getActivity().getApplicationContext(), feedMessagesList);
+
+        rssListView.setAdapter(messageAdapter);
+
+        rssListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                FeedMessage itemAtPosition = (FeedMessage) parent.getItemAtPosition(position);
+
+                Bundle bundle = new Bundle();
+                bundle.putString("url", itemAtPosition.getLink());
+
+                showOtherFragment(bundle);
+            }
+        });
+    }
+
+    private void initSearch() {
+        inputSearch = (EditText) view.findViewById(R.id.searchInput);
+
+        inputSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                messageAdapter.getFilter(s.toString());
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    private Feed getFeed() {
+        try {
+            feed = new RSSFeedParser().execute(RSS_URL).get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return feed;
+    }
+
+    public void showOtherFragment(Bundle bundle)
     {
         Fragment fr = new ViewerFragment();
+        fr.setArguments(bundle);
+
         FragmentChangeListener fc = (FragmentChangeListener) getActivity();
         fc.replaceFragment(fr);
     }
