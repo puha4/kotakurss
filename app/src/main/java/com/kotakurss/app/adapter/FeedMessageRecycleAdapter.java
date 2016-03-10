daf5391b321bde5553147becf8bc2502c992b9f7
Vitaliy Pugach <vitalick.pugach@gmail.com>
2016-03-10 12:30:46 +0200

    recycleview and cardview instead listview

---
+++ app/src/main/java/com/kotakurss/app/adapter/FeedMessageRecycleAdapter.java
@@ -0,0 +1,112 @@
+package com.kotakurss.app.adapter;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.kotakurss.app.R;
+import com.kotakurss.app.fragment.FragmentChangeListener;
+import com.kotakurss.app.fragment.ViewerFragment;
+import com.kotakurss.app.image.ImageLoader;
+import com.kotakurss.app.rss.FeedMessage;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class FeedMessageRecycleAdapter extends RecyclerView.Adapter<FeedMessageRecycleAdapter.FeedViewHolder> {
+    private List<FeedMessage> list;
+    private ArrayList<FeedMessage> container;
+    private ImageLoader imageLoader;
+    private Context context;
+
+    public FeedMessageRecycleAdapter(List<FeedMessage> list) {
+        this.list = list;
+        this.container = new ArrayList<FeedMessage>();
+        this.container.addAll(list);
+    }
+
+    @Override
+    public FeedViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.feed_item, viewGroup, false);
+
+        context = viewGroup.getContext();
+        imageLoader = new ImageLoader(viewGroup.getContext());
+
+        return new FeedViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(FeedViewHolder feedViewHolder, int i) {
+        final FeedMessage feedMessage = list.get(i);
+        feedViewHolder.title.setText(feedMessage.getTitle());
+        feedViewHolder.date.setText(feedMessage.getDate());
+
+        if (feedMessage.getImgUrl().isEmpty()) {
+            feedViewHolder.imageView.setVisibility(View.GONE);
+        } else {
+            feedViewHolder.imageView.setVisibility(View.VISIBLE);
+            imageLoader.DisplayImage(feedMessage.getImgUrl(), feedViewHolder.imageView);
+        }
+
+        feedViewHolder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle bundle = new Bundle();
+                bundle.putString("url", feedMessage.getLink());
+                showOtherFragment(bundle);
+            }
+        });
+    }
+
+    public void showOtherFragment(Bundle bundle)
+    {
+        Fragment fr = new ViewerFragment();
+        fr.setArguments(bundle);
+
+        FragmentChangeListener fc = (FragmentChangeListener) context;
+        fc.replaceFragment(fr);
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public static class FeedViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        TextView title;
+        TextView date;
+        ImageView imageView;
+
+        public FeedViewHolder(View itemView) {
+            super(itemView);
+
+            title = (TextView) itemView.findViewById(R.id.messageTitle);
+            date = (TextView) itemView.findViewById(R.id.messageDate);
+            imageView = (ImageView) itemView.findViewById(R.id.messagePicture);
+            cardView = (CardView) itemView.findViewById(R.id.cardView);
+        }
+    }
+
+    public void getFilter(String charText) {
+        charText = charText.toLowerCase(Locale.getDefault());
+        list.clear();
+        if (charText.length() == 0) {
+            list.addAll(container);
+        } else {
+            for (FeedMessage feedMessage : container) {
+                if (feedMessage.getTitle().toLowerCase(Locale.getDefault()).contains(charText)) {
+                    list.add(feedMessage);
+                }
+            }
+        }
+        notifyDataSetChanged();
+    }
+}
--- app/src/main/java/com/kotakurss/app/fragment/FeedListFragment.java
+++ app/src/main/java/com/kotakurss/app/fragment/FeedListFragment.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import com.kotakurss.app.R;
-import com.kotakurss.app.adapter.FeedMessageAdapter;
+import com.kotakurss.app.adapter.FeedMessageRecycleAdapter;
 import com.kotakurss.app.rss.Feed;
 import com.kotakurss.app.rss.FeedMessage;
 import com.kotakurss.app.rss.RSSFeedParser;
@@ -19,10 +19,10 @@ import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class FeedListFragment extends Fragment {
-    private ListView rssListView;
+    private RecyclerView recyclerView;
     private Feed feed;
     private View view;
-    private FeedMessageAdapter messageAdapter;
+    private FeedMessageRecycleAdapter messageAdapter;
     private SwipeRefreshLayout swipeRefreshLayout;
 
     private static final String RSS_URL = "http://kotaku.com/rss";
@@ -69,30 +69,16 @@ public class FeedListFragment extends Fragment {
     private void initRssList() {
         swipeRefreshLayout.setRefreshing(true);
 
-        rssListView = (ListView) view.findViewById(R.id.rssListView);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycleView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity().getApplicationContext()));
+
         feed = getFeed();
         List<FeedMessage> feedMessagesList = feed.getMessages();
 
-        messageAdapter = new FeedMessageAdapter(this.getActivity().getApplicationContext(), feedMessagesList);
-        rssListView.setAdapter(messageAdapter);
-
-        setOnListItemClickListener();
+        messageAdapter = new FeedMessageRecycleAdapter(feedMessagesList);
+        recyclerView.setAdapter(messageAdapter);
 
         swipeRefreshLayout.setRefreshing(false);
-    }
-
-    private void setOnListItemClickListener() {
-        rssListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                FeedMessage itemAtPosition = (FeedMessage) parent.getItemAtPosition(position);
-
-                Bundle bundle = new Bundle();
-                bundle.putString("url", itemAtPosition.getLink());
-
-                showOtherFragment(bundle);
-            }
-        });
     }
 
     public void setSearchQuery(String searchQuery) {
@@ -109,14 +95,5 @@ public class FeedListFragment extends Fragment {
         }
 
         return feed;
-    }
-
-    public void showOtherFragment(Bundle bundle)
-    {
-        Fragment fr = new ViewerFragment();
-        fr.setArguments(bundle);
-
-        FragmentChangeListener fc = (FragmentChangeListener) getActivity();
-        fc.replaceFragment(fr);
     }
 }
